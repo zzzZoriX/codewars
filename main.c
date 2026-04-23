6947310e86f2745e9a1d578fe3736418b09b6b89
anyway <zorix228337@gmail.com>
2026-04-23 20:31:33 +0300

    +2кю

--- main.c
+++ main.c
@@ -110,3 +110,22 @@ Examples
 //
 //     return 0;
 // }    COMPLETE!!!!!!!!!!!!!
+
+
+/*
+* Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+//
+// int unusual_five(void) {
+//     return '}' ^ 'x';
+// }
+//
+// int main() {
+//     printf("%d", (int)unusual_five());
+//
+//     return 0;
+// }    COMPLETE!!!!!!
